cb04a6a49c21ccd02a67ce55e8dd112974082816
kdb1248 <kdb1248@korea.ac.kr>
2025-03-07 16:51:35 +0900

    2. RestaurantMenuView (프론트&백엔드) 완료

--- Frontend/src/router/index.js
+++ Frontend/src/router/index.js
@@ -1,5 +1,6 @@
 import { createRouter, createWebHistory } from 'vue-router'
 import CustomerLoginView from '../views/CustomerLoginView.vue'
+import RestaurantMenuView from '../views/RestaurantMenuView.vue'
 
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
@@ -9,7 +10,11 @@ const router = createRouter({
       name: 'CustomerLoginView',
       component: CustomerLoginView,
     },
-    
+    {
+      path: '/restaurants/:restaurantId/tables/:tableId/menu',
+      name: 'RestaurantMenuView',
+      component: RestaurantMenuView
+    },
   ],
 })
 
