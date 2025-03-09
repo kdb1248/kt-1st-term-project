ebb467775643c8cf3d742c3bf25a5c25f882d6f9
kdb1248 <kdb1248@korea.ac.kr>
2025-03-09 17:53:20 +0900

    7. ownerViewLogin API(백엔드 완료)

---
+++ Backend/hiorder_backend/src/main/java/com/kt/hiorder_backend/dto/OwnerViewLoginRequest.java
@@ -0,0 +1,12 @@
+
+package com.kt.hiorder_backend.dto;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+public class OwnerViewLoginRequest {
+    private String restaurantName;
+}
+
